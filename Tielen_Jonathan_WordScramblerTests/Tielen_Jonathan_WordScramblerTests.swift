a0e438d7957f4df65f0a25bc27a69ec4966bac52
Jonathan Tielen <jonathant-991@hotmail.com>
2018-09-21 13:19:34 -0400

    Initial Commit

---
+++ Tielen_Jonathan_WordScramblerTests/Tielen_Jonathan_WordScramblerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tielen_Jonathan_WordScramblerTests.swift
+//  Tielen_Jonathan_WordScramblerTests
+//
+//  Created by Period Three on 9/21/18.
+//  Copyright © 2018 Period Three. All rights reserved.
+//
+
+import XCTest
+@testable import Tielen_Jonathan_WordScrambler
+
+class Tielen_Jonathan_WordScramblerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
